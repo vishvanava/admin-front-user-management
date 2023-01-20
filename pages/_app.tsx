d5ae6461271537c091faa0df2405d9f29db1c88d
vishvanava <vishva.navanjana@clouda.io>
2023-01-20 10:28:57 +0530

    add frontend files

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import './Styles.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layout/Layout'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
+}
+
+export default App
